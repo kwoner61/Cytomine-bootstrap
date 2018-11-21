5f338bbfd8712f74ca07263713017e2b7eed593f
Ulysse Rubens <ulyrubens@hotmail.com>
2018-11-21 17:37:46 +0100

    Delete old files

--- ims/setenv.sh
+++
@@ -1,1 +0,0 @@
-JAVA_OPTS="-Djava.awt.headless=true -server -Xms512m -Xmx4096m -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled"
