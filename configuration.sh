36147874aaf6927626f54bcce57b63014d526809
Ulysse Rubens <urubens@uliege.be>
2019-04-02 10:03:00 +0200

    Replace PATH_SOFTWAREROUTER_CONFIG example

--- configuration.sh
+++ configuration.sh
@@ -103,10 +103,9 @@ IRIS_ID="LOCAL_CYTOMINE_IRIS"
 SOFTWARE_ENABLED=true
 SOFTWARE_DEVELOPMENT=false
 PATH_SOFTWAREROUTER_CONFIG=
-
 #note for the developers: this path is the path which refer to your cytomine software router folder
 #so basically, you have to fill this field with the correct path.
-#example: PATH_SOFTWAREROUTER_CONFIG="${HOME}/IdeaProjects/Cytomine-software-router2"
+#example: PATH_SOFTWAREROUTER_CONFIG="${HOME}/Cytomine-ULiege/Cytomine-software-router"
 
 SOFTWARE_CODE_PATH=/data/softwares/code
 SOFTWARE_DOCKER_IMAGES_PATH=/data/softwares/images
